ecd425dc6cbaade09c73d405fdbb66ca3193ef74
David Cuero <W5845542@student.hccs.edu>
2020-05-05 14:05:04 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ import Map from 'components/Map';
 
 const LOCATION = {
   lat: 0,
-  lng: 0,
+  lng: 0
 };
 const CENTER = [LOCATION.lat, LOCATION.lng];
 const DEFAULT_ZOOM = 2;
 
 const IndexPage = () => {
-  
-/**
+
+  /**
    * mapEffect
    * @description Fires a callback once the page renders
    * @example Here this is and example of being used to zoom in and set a popup on load
@@ -26,7 +26,7 @@ const IndexPage = () => {
     let response;
 
     try {
-      response = await axios.get ('https://corona.lmao.ninja/v2/countries');
+      response = await axios.get('https://corona.lmao.ninja/v2/countries');
     } catch(e) {
       console.log(`Failed to fetch countries: ${e.message}`, e);
       return;
@@ -54,59 +54,59 @@ const IndexPage = () => {
         }
       })
     }
-    }
+
+    const geoJsonLayers = new L.GeoJSON(geoJson, {
+      pointToLayer: (feature = {}, latlng) => {
+        const { properties = {} } = feature;
+        let updatedFormatted;
+        let casesString;
+
+        const {
+          country,
+          updated,
+          cases,
+          deaths,
+          recovered
+        } = properties
+
+        casesString = `${cases}`;
+
+        if ( cases > 1000 ) {
+          casesString = `${casesString.slice(0, -3)}k+`
+        }
+
+        if ( updated ) {
+          updatedFormatted = new Date(updated).toLocaleString();
+        }
+
+        const html = `
+          <span class="icon-marker">
+            <span class="icon-marker-tooltip">
+              <h2>${country}</h2>
+              <ul>
+                <li><strong>Confirmed:</strong> ${cases}</li>
+                <li><strong>Deaths:</strong> ${deaths}</li>
+                <li><strong>Recovered:</strong> ${recovered}</li>
+                <li><strong>Last Update:</strong> ${updatedFormatted}</li>
+              </ul>
+            </span>
+            ${ casesString }
+          </span>
+        `;
+
+        return L.marker( latlng, {
+          icon: L.divIcon({
+            className: 'icon',
+            html
+          }),
+          riseOnHover: true
+        });
+      }
+    });
+
+    geoJsonLayers.addTo(map)
   }
 
-  const geoJsonLayers = new L.GeoJSON(geoJson, {
-    pointToLayer: (feature = {}, latlng) => {
-      const { properties = {} } = feature;
-      let updatedFormatted;
-      let casesString;
-
-      const {
-        country,
-        updated,
-        cases,
-        deaths,
-        recovered
-      } = properties
-
-      casesString = `${cases}`;
-
-      if ( cases > 1000 ) {
-        casesString = `${casesString.slice(0, -3)}k+`
-      }
-
-      if ( updated ) {
-        updatedFormatted = new Date(updated).toLocaleString();
-      }
-
-      const html = `
-        <span class="icon-marker">
-          <span class="icon-marker-tooltip">
-            <h2>${country}</h2>
-            <ul>
-              <li><strong>Confirmed:</strong> ${cases}</li>
-              <li><strong>Deaths:</strong> ${deaths}</li>
-              <li><strong>Recovered:</strong> ${recovered}</li>
-              <li><strong>Last Update:</strong> ${updatedFormatted}</li>
-            </ul>
-          </span>
-          ${ casesString }
-        </span>
-      `;
-
-      return L.marker( latlng, {
-        icon: L.divIcon({
-          className: 'icon',
-          html
-        }),
-        riseOnHover: true
-      });
-    }
-  });
-
-  geoJsonLayers.addTo(map)
   const mapSettings = {
     center: CENTER,
     defaultBaseMap: 'OpenStreetMap',
@@ -117,12 +117,12 @@ const IndexPage = () => {
   return (
     <Layout pageName="home">
       <Helmet>
-        <title>Home Page</title>
+        <title>COVID-19 Map</title>
       </Helmet>
 
       <Map {...mapSettings} />
-   
-    <Container type="content" className="text-center home-start">
+
+      <Container type="content" className="text-center home-start">
         <h2>Still Getting Started?</h2>
         <p>Run the following in your terminal!</p>
         <pre>
@@ -132,6 +132,6 @@ const IndexPage = () => {
       </Container>
     </Layout>
   );
-;
+};
 
 export default IndexPage;
